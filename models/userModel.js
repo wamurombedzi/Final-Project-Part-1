61ea767dc60a3d886bb5c5d78c388d7dca039c86
wamurombedzi <wadzamurombedzi@gmail.com>
2026-08-08 22:50:22 +0300

    Final project

---
+++ models/userModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import validator from "validator";
+//schema
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name Is Require"],
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: [true, " Email is Require"],
+      unique: true,
+      validate: validator.isEmail,
+    },
+    password: {
+      type: String,
+      required: [true, "password is require"],
+      minlength: [6, "Password length should be greater than 6 character"],
+      select: true,
+    },
+    location: {
+      type: String,
+      default: "Cyprus",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -1,15 +1,42 @@
-// imports
-const express = require('express');
+// package imports
+import express from "express";
+import dotenv from 'dotenv';
+import colors from 'colors';
+import cors from 'cors';
+import morgan from 'morgan';
+// files imports
+import connectDB from "./config/db.js";
+// routes imports
+import testRoutes from './routes/testRoutes.js';
+import authRoutes from './routes/authRoutes.js'
+import erroMiddleware from "./middlewares/erroMiddleware.js";
+
+// Dot ENV config
+dotenv.config();
+
+// mongodb connection
+connectDB();
 
 // rest object
 const app = express();
 
+// middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
 // routes 
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to FINAL PROJECT PART 1</h1>")
-});
+app.use('/api/v1/test', testRoutes);
+app.use('/api/v1/auth', authRoutes);
+
+//validation middleware
+app.use(erroMiddleware);
+
+//port
+const PORT = process.env.PORT || 3000;
 
 // listen
 app.listen(3000, () => {
-  console.log("Node Server Running on port 3000");
+  console.log(`Node Server Running In ${process.env.DEV_MODE} Mode on port on ${PORT}`
+  );
 });
